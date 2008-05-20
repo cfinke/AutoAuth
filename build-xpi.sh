7257c32711f827abca2a94dfa9c4b7c9782baa9f
cfinke <cfinke@gmail.com>
2008-05-20 18:27:51 +0000

    Import: version 1.2.3

---
+++ build-xpi.sh
@@ -0,0 +1,21 @@
+rm -rf `find ./ -name ".DS_Store"`
+rm -rf `find ./ -name "Thumbs.db"`
+rm autoauth.xpi
+rm -rf .tmp_xpi_dir/
+
+chmod -R 0777 autoauth/
+
+mkdir .tmp_xpi_dir/
+cp -r autoauth/* .tmp_xpi_dir/
+rm -rf `find ./.tmp_xpi_dir/ -name ".svn"`
+
+cd .tmp_xpi_dir/chrome/
+zip -rq ../autoauth.jar *
+rm -rf *
+mv ../autoauth.jar ./
+cd ../
+zip -rq ../autoauth.xpi *
+cd ../
+rm -rf .tmp_xpi_dir/
+
+cp autoauth.xpi ~/Desktop/
