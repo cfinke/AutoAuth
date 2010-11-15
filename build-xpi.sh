a5b48004febb312e4178628589811639bda28608
Christopher Finke <cfinke@gmail.com>
2010-11-15 02:33:22 +0000

    Removing .git directories when building

--- build-xpi.sh
+++ build-xpi.sh
@@ -7,7 +7,7 @@ chmod -R 0777 autoauth/
 
 mkdir .tmp_xpi_dir/
 cp -r autoauth/* .tmp_xpi_dir/
-rm -rf `find ./.tmp_xpi_dir/ -name ".svn"`
+rm -rf `find ./.tmp_xpi_dir/ -name ".git"`
 
 cd .tmp_xpi_dir/chrome/
 zip -rq ../autoauth.jar *
