359da29cce9f4d1237cea61d087c41e952d9b4e3
Christopher Finke <cfinke@gmail.com>
2010-11-17 03:40:09 +0000

    No longer double-jarring

--- build-xpi.sh
+++ build-xpi.sh
@@ -1,6 +1,7 @@
 rm -rf `find ./ -name ".DS_Store"`
 rm -rf `find ./ -name "Thumbs.db"`
-rm autoauth.xpi
+
+rm ~/Desktop/autoauth.xpi
 rm -rf .tmp_xpi_dir/
 
 chmod -R 0777 autoauth/
@@ -9,13 +10,7 @@ mkdir .tmp_xpi_dir/
 cp -r autoauth/* .tmp_xpi_dir/
 rm -rf `find ./.tmp_xpi_dir/ -name ".git"`
 
-cd .tmp_xpi_dir/chrome/
-zip -rq ../autoauth.jar *
-rm -rf *
-mv ../autoauth.jar ./
-cd ../
-zip -rq ../autoauth.xpi *
+cd .tmp_xpi_dir/
+zip -rq ~/Desktop/autoauth.xpi *
 cd ../
 rm -rf .tmp_xpi_dir/
-
-cp autoauth.xpi ~/Desktop/
